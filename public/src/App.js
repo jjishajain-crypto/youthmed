6881dfe8d2ae0aac17b54464e7c33fe686f23957
jjishajain-crypto <j.jishajain@gmail.com>
2025-10-21 22:08:41 -0700

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Join from './components/Join';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Join />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
