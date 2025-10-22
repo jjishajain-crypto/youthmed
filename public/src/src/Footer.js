c283f8047a06e6385638cac475f8125734802077
jjishajain-crypto <j.jishajain@gmail.com>
2025-10-21 22:10:12 -0700

    Create Footer.js

---
+++ public/src/src/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer id="contact" style={{textAlign:'center', padding:'3rem 1rem', backgroundColor:'#ff69b4', color:'#fff'}}>
+      <h4>Connect with YouthMed</h4>
+      <p>Have ideas or want to collaborate? Reach out to us anytime!</p>
+      <p>Email: <a href="mailto:youthmed.org@gmail.com" style={{color:'#fff', textDecoration:'underline'}}>youthmed.org@gmail.com</a></p>
+      <p style={{marginTop:'1rem', fontSize:'0.8rem'}}>© 2025 YouthMed. All rights reserved.</p>
+    </footer>
+  );
+}
